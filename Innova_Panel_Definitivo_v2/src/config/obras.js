b0610ef3526bcc38cf3a2decd6b6940dc8e0aaca
adrial1699-art <adrial1699@gmail.com>
2025-12-16 00:04:49 +0100

    Add files via upload

---
+++ Innova_Panel_Definitivo_v2/src/config/obras.js
@@ -0,0 +1,6 @@
+
+export const OBRAS = {
+  "Innova Beach III": { plano:"/planes/innova_iii.png" },
+  "Innova Beach IV": { plano:"/planes/innova_iv.png" },
+  "Innova Thiar": { plano:"/planes/innova_thiar.png" }
+}
