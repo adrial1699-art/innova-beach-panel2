32db6b443bcf21ea76a0d07a894b96b625b0d8bb
adrial1699-art <adrial1699@gmail.com>
2025-12-18 22:00:24 +0100

    Add files via upload

--- src/config/obras.js
+++ src/config/obras.js
@@ -1,6 +1,30 @@
-// obras.js
-export const OBRAS = [
-  "INNOVA BEACH III",
-  "INNOVA BEACH IV",
-  "INNOVA THIAR"
-];
+
+export const SHEET_ID = "17aB2MrWCG573pSNPatGqQ89UglR0mhCokGb1C0CG7bw";
+
+export const OBRAS = {
+  "INNOVA BEACH III": {
+    sheetName: "INNOVA BEACH III",
+    bloques: {
+      "Bloque 1": [1,2,3,4,5,6],
+      "Bloque 2": [7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],
+      "Dúplex": [23,24]
+    }
+  },
+  "INNOVA BEACH IV": {
+    sheetName: "INNOVA BEACH IV",
+    bloques: {
+      "Bloque 1": [1,2,3,4,5,6,7,8,9,10],
+      "Bloque 2": [11,12,13,14,15,16,17,18,19,20,21,22]
+    }
+  },
+  "INNOVA THIAR": {
+    sheetName: "INNOVA THIAR",
+    bloques: {
+      "Bloque 1": [1,2,3,4,5,6,7,8],
+      "Bloque 2": [9,10,11,12,13,14,15,16],
+      "Bloque 3": [17,18,19,20,21,22,23,24,25,26,27,28],
+      "Bloque 4": [29,30,31,32,33,34],
+      "Bloque 5": [35,36,37,38,39,40,41,42,43,44]
+    }
+  }
+};
