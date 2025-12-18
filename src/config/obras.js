4cfc096da197c5d1883e19a5e202396679c4ec34
adrial1699-art <adrial1699@gmail.com>
2025-12-18 12:33:14 +0100

    Update obras.js

--- src/config/obras.js
+++ src/config/obras.js
@@ -1,6 +1,6 @@
 
 export const OBRAS = {
-  "Innova Beach III": { plano:"/planes/innova_iii.png" },
-  "Innova Beach IV": { plano:"/planes/innova_iv.png" },
-  "Innova Thiar": { plano:"/planes/innova_thiar.png" }
+  "INNOVA BEACH III": { plano:"/planes/innova_iii.png" },
+  "INNOVA BEACH IV": { plano:"/planes/innova_iv.png" },
+  "INNOVA THIAR": { plano:"/planes/innova_thiar.png" }
 }
